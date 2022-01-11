13b4c3cfb1c4b69d6cee01421487ec2e8102aa74
Alberto Angel Cruz <37281529+angelcruzalv@users.noreply.github.com>
2022-01-10 23:52:27 -0600

    First commit

---
+++ first-ios-app/first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ios_appApp.swift
+//  first-ios-app
+//
+//  Created by Angel Cruz on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
